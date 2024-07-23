b415b9ad49a2ad3ea6e8ff694da7a6e509be137e
Kipchumba C. Bett <kbett68@gmail.com>
2024-07-23 10:04:10 +0300

    Add ViralLoadRequestPayload model

---
+++ ettors-openmrs-mapper/src/main/java/org/ephi/ettors/model/ViralLoadRequestPayload.java
@@ -0,0 +1,127 @@
+package org.ephi.ettors.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViralLoadRequestPayload {
+
+    @JsonProperty("MRN")
+    private String mrn;
+
+    @JsonProperty("UAN")
+    private String uan;
+
+    @JsonProperty("FacilityCode")
+    private String facilityCode;
+
+    @JsonProperty("RequestBy")
+    private String requestedBy;
+
+    @JsonProperty("requestedDate")
+    private String requestedDate;
+
+    @JsonProperty("Regimen")
+    private String regimen;
+
+    @JsonProperty("DateInitiated")
+    private String dateInitiated;
+
+    @JsonProperty("Pregnancy")
+    private String pregnancy;
+
+    @JsonProperty("Breastfeeding")
+    private String breastfeeding;
+
+    @JsonProperty("CD4MostRecent")
+    private String cd4MostRecent;
+
+    @JsonProperty("CD4MostRecentDate")
+    private String cd4MostRecentDate;
+
+    @JsonProperty("RoutineVL")
+    private String routineVL;
+
+    @JsonProperty("RoutineVLPregnantMother")
+    private String RoutineVLPregnantMother;
+
+    @JsonProperty("Targeted")
+    private String targeted;
+
+    @JsonProperty("SpecimentCollectedDate")
+    private String specimenCollectedDate;
+
+    @JsonProperty("SpecimenType")
+    private String specimenType;
+
+    @JsonProperty("SpecimenSentToReferalDate")
+    private String specimenSentToReferralDate;
+
+    @JsonProperty("LabId")
+    private String labId;
+
+    @JsonProperty("LabName")
+    private String labName;
+
+    @JsonProperty("SpecimenRecievedDate")
+    private String specimenReceivedDate;
+
+    @JsonProperty("SpecimenQuality")
+    private String specimenQuality;
+
+    @JsonProperty("ReasonForRejection")
+    private String reasonForRejection;
+
+    @JsonProperty("TestResultDate")
+    private String testResultDate;
+
+    @JsonProperty("TestResult")
+    private String testResult;
+
+    @JsonProperty("TestedBy")
+    private String testedBy;
+
+    @JsonProperty("ReviewedBy")
+    private String reviewedBy;
+
+    @JsonProperty("AletSentDate")
+    private String alertSentDate;
+
+    @JsonProperty("DispatchDate")
+    private String dispatchDate;
+
+    @JsonProperty("ResultReachedToFacDate")
+    private String resultReachedToFacDate;
+
+    @JsonProperty("ResultReceivedByFacility")
+    private String resultReceivedByFacility;
+
+    @JsonProperty("AttachedToPatientDate")
+    private String attachedToPatientDate;
+
+    @JsonProperty("CommunicatedToPatientDate")
+    private String communicatedToPatientDate;
+
+    @JsonProperty("RequestID")
+    private String requestID;
+
+    @JsonProperty("ResponseID")
+    private String responseID;
+
+    @JsonProperty("FacilityName")
+    private String facilityName;
+
+    @JsonProperty("RegionName")
+    private String regionName;
+
+    @JsonProperty("Sex")
+    private String sex;
+
+    @JsonProperty("Age")
+    private String age;
+
+    @JsonProperty("AgeInMonths")
+    private String ageInMonths;
+}
