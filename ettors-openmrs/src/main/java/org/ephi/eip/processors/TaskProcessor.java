7f12bad0d7139c0b7da1bd33afcad2a3830e3b1f
Kipchumba C. Bett <kbett68@gmail.com>
2024-07-24 12:26:29 +0300

    Process task (associate it with order)

--- ettors-openmrs/src/main/java/org/ephi/eip/processors/TaskProcessor.java
+++ ettors-openmrs/src/main/java/org/ephi/eip/processors/TaskProcessor.java
@@ -14,7 +14,10 @@ public class TaskProcessor implements Processor {
     @Override
     public void process(Exchange exchange) {
         ServiceRequest serviceRequest = exchange.getProperty(Constants.EXCHANGE_PROPERTY_SERVICE_REQUEST, ServiceRequest.class);
+        String orderNumber = exchange.getProperty(Constants.EXCHANGE_PROPERTY_ORDER_NUMBER, String.class);
         Task task = new Task();
+        // Add order number as an identifier to the task
+        task.addIdentifier().setSystem(Constants.IDENTIFIER_SYSTEM_ORDER_NUMBER).setValue(orderNumber);
         task.setStatus(Task.TaskStatus.REQUESTED);
         task.setIntent(Task.TaskIntent.ORDER);
         task.setPriority(Task.TaskPriority.ROUTINE);
@@ -23,10 +26,9 @@ public class TaskProcessor implements Processor {
         task.setAuthoredOn(serviceRequest.getAuthoredOn());
         task.setRequester(serviceRequest.getRequester());
         task.setCode(serviceRequest.getCode());
-
         // Add basedOn reference
         Reference basedOn = new Reference();
-        basedOn.setReference(serviceRequest.getId());
+        basedOn.setReference("ServiceRequest/" + serviceRequest.getIdPart());
         task.addBasedOn(basedOn);
         exchange.getMessage().setBody(task);
     }
